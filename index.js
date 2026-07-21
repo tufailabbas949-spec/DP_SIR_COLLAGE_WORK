f3bfe42aee6987088cbb0e9edb8cb252498bb984
tufail abbas <tufailabbas949@gmail.com>
2026-07-21 14:11:59 -0700

    destucturing, array map filter,spread op

--- index.js
+++ index.js
@@ -64,8 +64,51 @@ console.log(s_nmae,s_age)
 const{named,aged}= student;
 console.log(name,age)
 
-
-
-
-
 // destucturing  array
+ // old way
+ const namelist = ["tufail","abbas","rizvi","ali"]
+const fisrtname = namelist[0]
+const secondname = namelist[1]
+const thiredname = namelist[2]
+console.log(fisrtname)
+console.log(secondname)
+console.log(thiredname)
+ console.log(namelist)
+
+ // new way 
+ const [f_name,s1_name,t_name] = namelist
+ console.log(f_name)
+ console.log(s1_name)
+ console.log(t_name)
+
+ //array map filter
+ let number =  [1,2,3,4,5,6]
+ let new_num = []
+ for(let i = 0; i<=(number.length-1) ; i++){
+    new_num.push(number[i]+2)
+ }
+ console.log(new_num)
+ console.log(typeof(number))
+
+ // use map 
+ let new_array =  number.map((n)=>{
+                return n+2
+  })
+   console.log(new_array)
+
+// use filter 
+  let n = number.filter((n)=>{
+        return n>=5
+ })
+  console.log(n)
+
+  // spread operator
+  //old way
+  function sumallnum(...val){
+        let sum = 0
+         for(let i=0;i<=val.length-1; i++){
+            sum = sum + val[i]
+         }
+        console.log(sum)
+  }
+sumallnum(10,20,50)
