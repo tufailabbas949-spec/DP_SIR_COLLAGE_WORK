f98a5a5514dc5259ee71364b2d3142cf5a34fcfa
tufail abbas <tufailabbas949@gmail.com>
2026-08-18 11:34:14 -0700

    inout focus use hook useRef

--- form/form_p/src/App.jsx
+++ form/form_p/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import {useState,useEffect} from 'react'
+import {useState,useEffect,useRef} from 'react'
 function App() {
-
+const inputRef = useRef(null);
+const handleFocus = () => {
+  inputRef.current.focus();
+}
 const [name, setName] = useState(()=>{
   return localStorage.getItem('name') || '';
 });
@@ -24,12 +27,18 @@ const handleSubmit = (e) => {
     <>
       <h1 className='text-center bg-red-500 font-bold'>form enter your details</h1>
       <form className='flex flex-col items-center justify-center mt-10' onSubmit={(e) => handleSubmit(e)}>
-        <input value={name} onChange={(e) => setName(e.target.value)}
+        <input value={name} ref={inputRef} onChange={(e) => setName(e.target.value)}
           type="text" placeholder='Enter your name' className='border-2 border-gray-300 rounded-md p-2 mb-4 w-1/3' />
         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='Enter your email' className='border-2 border-gray-300 rounded-md p-2 mb-4 w-1/3' />
         <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Enter your password' className='border-2 border-gray-300 rounded-md p-2 mb-4 w-1/3' />
-        <button type="submit"
+       <div className='flex justify-center'>
+         <button type="submit"
          className='bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600'>Submit</button>
+         <button type="reset" onClick={() => {setName(''); setEmail(''); setPassword('');}}
+         className='bg-red-500 text-white font-bold py-2 px-4 rounded-md hover:bg-red-600 ml-4'>Reset</button>  
+         <button type="button" className='bg-green-500 text-white font-bold py-2 px-4 rounded-md hover:bg-green-600 ml-4' 
+          onClick={handleFocus}>focus input</button>
+       </div>
       </form>
       <div className='mt-10 text-center'>
         <p>Name: {name}</p>
